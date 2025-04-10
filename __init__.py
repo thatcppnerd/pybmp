a62fe091d04d9e00856e6ef2a9e31839986dc757
thatcppnerd <welcometoheccmp4@gmail.com>
2025-04-10 18:28:18 -0500

    Structs are packed

--- __init__.py
+++ __init__.py
@@ -6,33 +6,43 @@ from ctypes import c_uint64 as u64
 
 
 class bmp_file_header(ct.Structure):
-    signature:  u16 = 0
-    filesize:   u32 = 0
-    reserved:   u32 = 0
-    offset:     u32 = 0
-
+    _pack_ = 1
+    
+    _fields_ = [
+        ("signature",  u16),
+        ("filesize",   u32),
+        ("reserved",   u32),
+        ("offset",     u32)
+    ]
     # def __init__(bytes: by)
 
 class bmp_info_header(ct.Structure):
-    size:               u32
-    width:              u32
-    height:             u32
-    planes:             u16
-    bits_per_pixel:     u16
-    compression:        u32
-    image_size:         u32
-    hori_res:           u32
-    vert_res:           u32
-    colors_used:        u32
-    important_colors:   u32
+    _pack_ = 1
+
+    _fields_ = [
+        ("size",                u32),
+        ("width",               u32),
+        ("height",              u32),
+        ("planes",              u16),
+        ("bits_per_pixel",      u16),
+        ("compression",         u32),
+        ("image_size",          u32),
+        ("hori_res",            u32),
+        ("vert_res",            u32),
+        ("colors_used",         u32),
+        ("important_colors",    u32)
+    ]
 
 class bmp_color_table:
     class rgb:
-        red:        u8
-        green:      u8
-        blue:       u8
-        reserved:   u8
-    
+        _pack_ = 1
+
+        _fields_ = [
+            ("red",         u8),
+            ("green",       u8),
+            ("blue",        u8),
+            ("reserved",    u8)
+        ]
     color: None
     
 class bmp_pixel_data:
@@ -41,7 +51,11 @@ class bmp_pixel_data:
 
 
 class bmp_file: 
-    file_header: bmp_file_header
-    info_header: bmp_info_header
-    color_table: bmp_color_table
-    pixel_data: bmp_pixel_data
+    _pack_ = 1
+
+    _fields_ = [
+        ("file_header", bmp_file_header),
+        ("info_header", bmp_info_header),
+        ("color_table", bmp_color_table),
+        ("pixel_data",  bmp_pixel_data)
+    ]
