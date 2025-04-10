075705e8888bd35e8e07f4837e43a447411a899c
thatcppnerd <welcometoheccmp4@gmail.com>
2025-04-10 10:27:45 -0500

    Reorganized

--- __init__.py
+++ __init__.py
@@ -4,37 +4,39 @@ from numpy import uint32 as u32
 from numpy import uint64 as u64
 
 
+class bmp_header:
+    signature:  u16
+    filesize:   u32
+    reserved:   u32
+    offset:     u32
+
+class bmp_info_header:
+    size:               u32
+    width:              u32
+    height:             u32
+    planes:             u16
+    bits_per_pixel:     u16
+    compression:        u32
+    image_size:         u32
+    hori_res:           u32
+    vert_res:           u32
+    colors_used:        u32
+    important_colors:   u32
+
+class bmp_color_table:
+    class rgb:
+        red:        u8
+        green:      u8
+        blue:       u8
+        reserved:   u8
+    
+    color: None
+    
+class bmp_pixel_data:
+    pixels: None
+
 class bmp_file:
-    class _header:
-        signature:  u16
-        filesize:   u32
-        reserved:   u32
-        offset:     u32
-
-    class _info_header:
-        size:               u32
-        width:              u32
-        height:             u32
-        planes:             u16
-        bits_per_pixel:     u16
-        compression:        u32
-        image_size:         u32
-        hori_res:           u32
-        vert_res:           u32
-        colors_used:        u32
-        important_colors:   u32
-
-    class _color_table:
-        class rgb:
-            red:        u8
-            green:      u8
-            blue:       u8
-            reserved:   u8
-        
-        color: None
-        
-    class _pixel_data:
-        pixels: None
-
-        
-
+    header: bmp_header
+    info_header: bmp_info_header
+    color_table: bmp_color_table
+    pixel_data: bmp_pixel_data
